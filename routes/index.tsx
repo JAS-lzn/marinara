36d4ebe816385f91c17c611ba4587ca4216bd2ec
JAS-lzn <Joshua.Shearer64@gmail.com>
2024-01-11 21:40:34 -0600

    add the sauce

--- routes/index.tsx
+++ routes/index.tsx
@@ -29,14 +29,14 @@ export default function Home() {
 
 	return (
 		<div>
-			<div class="px-4 py-8 mx-auto bg-[#F08B86]">
+			<div id='banner' class="px-4 py-8 mx-auto bg-[#e0aa7e]">
 				<div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
 					<img
 						class="my-6"
-						src="/logo.svg"
+						src="/sauce.png"
 						width="128"
 						height="128"
-						alt="the Fresh logo: a sliced lemon dripping with juice"
+						alt="the Marinara logo: a stylized jar of marinara sauce with sauce and basil coming out the mouth of the jar"
 					/>
 					<h1 class="text-4xl font-bold">Marinara</h1>
 					<p class="my-4">
