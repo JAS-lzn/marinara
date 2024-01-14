15e9128c47f4636bc3491d0026775f4595ade9d4
JAS-lzn <Joshua.Shearer64@gmail.com>
2024-01-14 17:17:19 -0600

    fix the damn css

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component }: PageProps) {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>marinara</title>
 				<link rel="stylesheet" href="/styles.css" />
+				<link rel="stylesheet" href="/tailwind_generated.css" />
 			</head>
 			<body>
 				<Component />
